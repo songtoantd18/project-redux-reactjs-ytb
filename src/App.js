c3f3f79c87395ca5e4ae02dfc02d46dc264cd4df
songtoantd18 <songtoantd18@gmail.com>
2023-01-01 15:11:13 +0700

    demo2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from "react";
-import HomePage from "./components/HomePage";
 
 const App = () => {
-  return (
-    <div>
-      <HomePage />
-    </div>
-  );
+  return <div>123</div>;
 };
 
 export default App;
